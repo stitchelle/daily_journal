fef859f9ae5fd74e2b44c432cc92c5acdcf24206
Michelle Johnson <bob1234girl@gmail.com>
2019-11-01 12:02:22 -0500

    finished refactoring for part 1

--- src/scripts/htmlToDom.js
+++ src/scripts/htmlToDom.js
@@ -1,31 +1,35 @@
 export default {
     buildAndAppendForm: () => {
         const form = `
+            <h1>Daily Journal</h1>
+            
             <form action="">
-                    <h1>Daily Journal</h1>
-                    <fieldset>
-                        <label for="journalDate">Date of Entry</label>
-                        <input type="date" name="journalDate" id="journalDate">
-                    </fieldset>
-                    <fieldset>
-                        <label for="conceptsCovered">Concepts Covered</label>
-                        <input type="text" name="conceptsCovered" id="conceptsCovered">
-                    </fieldset>
-                    <fieldset>
-                        <label for="journalEntry">Journal Entry</label>
-                        <textarea name="journalEntry" id="journalEntry" cols="20" rows="1"></textarea>
-                    </fieldset>
-                    <fieldset>
-                        <label for="journalEntry">Mood of the day</label>
-                        <select name="mood" id="mood">
+                <fieldset>
+                    <label id="date" for="journalDate">Date of Entry</label>
+                    <input type="date" name="journalDate" id="journalDate">
+                </fieldset>
+                <fieldset>
+                    <label id="concepts" for="conceptsCovered">Concepts Covered</label>
+                    <input type="text" name="conceptsCovered" id="conceptsCovered">
+                </fieldset>
+                <fieldset>
+                    <label id="entry" for="journalEntry">Journal Entry</label>
+                    <textarea name="journalEntry" id="journalEntry" cols="20" rows="1"></textarea>
+                </fieldset>
+                <fieldset>
+                    <label id="moodOfTheDay" for="journalEntry">Mood of the day</label>
+                    <select name="mood" id="mood">
                         <option value="happy">Happy</option>
                         <option value="sad">Sad</option>
                         <option value="mad">Mad</option>
                         <option value="okay">Okay</option>
-                        </select>
-                    </fieldset>
-                    <button id="recordButton">Record Journal Entry</button>        
+                    </select>
+                </fieldset>
             </form>
+
+            <button id="recordButton">Record Journal Entry</button>  
+
+            <hr/>
             `
 
         const formContainerDiv = document.querySelector("#form-container")
