6c3c78a4b282a18b0f0089b848a248cd209df6bf
Michelle Johnson <bob1234girl@gmail.com>
2019-11-03 14:33:48 -0600

    added empty article tag for entries

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,30 +1,37 @@
-// import buildAndAppendForm from "./formMaker.js"
+import buildAndAppendForm from "./formMaker.js"
 
-// buildAndAppendForm.buildAndAppendForm()
+buildAndAppendForm.buildAndAppendForm()
 
 
 
-    const journalEntry = [{
-        date: "2019/09/30",
-        concept: "Basic Terminal Commands",
-        entry: "We created out GitHub accounts and learned basic terminal commands.",
-        mood: "OK"
-    },
-    {
-        date: "2019/10/01",
-        concept: "Chapters 2,3 and 4",
-        entry: "Command Line, Styling with CSS and Semantic HTML components",
-        mood: "OK"
-    },
-    {
-        date: "2019/10/02",
-        concept: "Chapter 6",
-        entry: "Intro to Git snd DVCS",
-        mood: "OK"
-    }]
+const journalEntry = [{
+    date: "2019/09/30",
+    concept: "Basic Terminal Commands",
+    entry: "We created out GitHub accounts and learned basic terminal commands.",
+    mood: "OK"
+},
+{
+    date: "2019/10/01",
+    concept: "Chapters 2,3 and 4",
+    entry: "Command Line, Styling with CSS and Semantic HTML components",
+    mood: "OK"
+},
+{
+    date: "2019/10/02",
+    concept: "Chapter 6",
+    entry: "Intro to Git snd DVCS",
+    mood: "OK"
+}]
 
-    const makeJournalEntryComponent = (journalEntry) {
-        return`
+/*
+Purpose: To create, and return, a string template that
+represents a single journal entry object as HTML
+
+Arguments: journalEntry (object)
+*/
+
+const makeJournalEntryComponent = (journalEntry) {
+    return `
         <section>
         <h3>${journalEntry.concept}</h3>
         <p>${journalEntry.entry}</p>
@@ -32,4 +39,4 @@
         <p>${journalEntry.date}</p>
       </section>
     `
-    }
+}
