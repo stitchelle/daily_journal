0159a938d138f6a474ef48d1fd15df97f8b25fa7
Michelle Johnson <bob1234girl@gmail.com>
2019-11-04 19:20:15 -0600

    completed part 5 of daily journal

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -6,6 +6,16 @@ import renderJournalEntry from "./entriesDom.js"
 
 buildAndAppendForm.buildAndAppendForm()
 
+/* ---what I originally did---
 API.API.getJournalEntries()
 
 .then(response => renderJournalEntry.renderJournalEntry(response))
+*/
+/*
+    Main application logic that uses the functions and objects
+    defined in the other JavaScript files.
+
+    Change the fake variable names below to what they should be
+    to get the data and display it.
+*/
+API.API.getJournalEntries().then(response => renderJournalEntry.renderJournalEntry(response))
